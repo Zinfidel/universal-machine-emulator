84df3307b46fdf60583fc919d02e21f29ea869bd
teberger <tberge01@cs.unm.edu>
2014-08-31 16:03:27 -0600

    /home/taylor/git/universal-machine-emulator/main.c autocommit from emacs: Sun Aug 31 16:03:27 2014

--- main.c
+++ main.c
@@ -354,11 +354,10 @@ int LoadProgram(Instruction inst) {
   MemArray* mem = (MemArray*) Registers[inst.registerB];
   uint32_t offset = Registers[inst.registerC];
 
-  //TODO: How to manage these? How to find the size?
   // If the program is just using this instruction to move the program
   // counter, don't bother with copying memory and stuff.
   if(mem == 0) {
-    ProgramCounter = program_array->array + offset;
+    ProgramCounter = (program_array->array) + offset;
     return RET_SUCCESS;
   }
 

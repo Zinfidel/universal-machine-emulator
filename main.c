8bca139c8f642bec8405e380b394d3e2e179a2f2
teberger <tberge01@cs.unm.edu>
2014-08-31 14:40:52 -0600

    /home/taylor/git/universal-machine-emulator/main.c autocommit from emacs: Sun Aug 31 14:40:52 2014

--- main.c
+++ main.c
@@ -77,12 +77,12 @@ int Init(int argc, char **argv) {
   }
 
   
-  MemArray* global_init;
-  global_init->size = -1;
-  global_init->array = NULL;
+  MemArray global_init;
+  global_init.size = -1;
+  global_init.array = NULL;
 
   // Try to load the file into array 0 and point the counter to it.
-  LoadFile(argv[1], global_init);
+  LoadFile(argv[1], &global_init);
   ProgramCounter = global_init->array;
   
   if(global_init->size = -1)
